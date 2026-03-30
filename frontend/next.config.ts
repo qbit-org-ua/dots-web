c34e27e6b2c6dde5db0bf47662f3c1927d2b27af
DOTS Developer <dev@dots.org.ua>
2026-03-30 22:41:33 +0200

    chore: Use testing domain name for backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['127.0.0.1'],
   async rewrites() {
     return [
-      { source: '/api/:path*', destination: 'http://172.17.0.5:3001/api/:path*' },
+      { source: '/api/:path*', destination: 'https://xto.dots.org.ua/api/:path*' },
     ];
   },
 };
